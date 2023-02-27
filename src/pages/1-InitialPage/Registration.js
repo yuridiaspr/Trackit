b6b97b59b328cc2cc53f6f74d78721e4d9c47e4b
Yuri Dias <yuridiaspr@gmail.com>
2023-02-26 22:59:38 -0300

    refactor: SignUp page

--- src/pages/1-InitialPage/Registration.js
+++ src/pages/1-InitialPage/Registration.js
@@ -11,61 +11,85 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useState } from "react";
 import { URL_Register } from "../../constants/urls";
+import { ThreeDots } from "react-loader-spinner";
 
 export default function Registration() {
   const navigate = useNavigate();
 
-  const [Email, setEmail] = useState("");
-  const [Password, setPassword] = useState("");
-  const [Name, setName] = useState("");
-  const [PictureURL, setPictureURL] = useState("");
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+    name: "",
+    image: "",
+  });
+  const [isLoading, setIsLoading] = useState(false);
 
-  function SubmitData() {
+  function handleForm(e) {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  }
+
+  function handleSignUp(e) {
+    e.preventDefault();
+    setIsLoading(true);
+
     axios
-      .post(URL_Register, {
-        email: Email,
-        name: Name,
-        image: PictureURL,
-        password: Password,
+      .post(URL_Register, form)
+      .then(() => {
+        setIsLoading(false);
+        navigate("/");
       })
-      .then(() => navigate("/"))
-      .catch((err) => alert(err.response.data.message));
+      .catch((err) => {
+        setIsLoading(false);
+        alert(err.response.data.message);
+      });
   }
 
   return (
     <Container>
       <MainLogo src={Logo} />
-      <StyledForm>
+      <StyledForm onSubmit={handleSignUp}>
         <StyledInput
-          data-identifier="input-email"
-          placeholder="   email"
+          name="email"
+          placeholder="email"
           type="email"
-          value={Email}
-          onChange={(e) => setEmail(e.target.value)}
+          required
+          disabled={isLoading}
+          value={form.email}
+          onChange={handleForm}
         />
         <StyledInput
-          data-identifier="input-password"
-          placeholder="   senha"
+          name="password"
+          placeholder="senha"
           type="password"
-          value={Password}
-          onChange={(e) => setPassword(e.target.value)}
+          required
+          disabled={isLoading}
+          value={form.password}
+          onChange={handleForm}
         />
         <StyledInput
-          data-identifier="input-name"
-          placeholder="   nome"
+          name="name"
+          placeholder="nome"
           type="text"
-          value={Name}
-          onChange={(e) => setName(e.target.value)}
+          required
+          disabled={isLoading}
+          value={form.name}
+          onChange={handleForm}
         />
         <StyledInput
-          data-identifier="input-photo"
-          placeholder="   foto"
+          name="image"
+          placeholder="foto"
           type="url"
-          value={PictureURL}
-          onChange={(e) => setPictureURL(e.target.value)}
+          required
+          disabled={isLoading}
+          value={form.image}
+          onChange={handleForm}
         />
-        <StyledButton onClick={() => SubmitData()} type="submit">
-          Cadastrar
+        <StyledButton type="submit" disabled={isLoading}>
+          {isLoading ? (
+            <ThreeDots width={50} height={50} color="#FFFFFF" />
+          ) : (
+            "Cadastrar"
+          )}
         </StyledButton>
       </StyledForm>
       <Register to="/" data-identifier="back-to-login-action">
